27c8a0f351c382766ed0eccb98ea896c1c27c377
amitthk <e.amitthakur@gmail.com>
2020-08-09 19:20:59 +0800

    margins and axes

--- app/D3Chart.js
+++ app/D3Chart.js
@@ -8,21 +8,28 @@ const datarr = [{'name': 'bob', 'points': 9},
 {'name': 'peter', 'points':32}]
 const data = [10,15,20,25,30]
 
-const WIDTH = 640
-const HEIGHT = 480
+const MARGINS = { TOP: 12, BOTTOM: 20, LEFT: 10, RIGHT: 5}
+const WIDTH = 640 - MARGINS.LEFT - MARGINS.RIGHT
+const HEIGHT = 480 - MARGINS.TOP - MARGINS.BOTTOM
 
 export default class D3Chart{
     constructor(element){
         var svg= d3.select(element)
         .append("svg")
-        .attr("width",WIDTH)
-        .attr("height",HEIGHT)
+        .attr("width",WIDTH + MARGINS.LEFT + MARGINS.RIGHT)
+        .attr("height",HEIGHT + MARGINS.TOP + MARGINS.BOTTOM)
 
         //d3.json(url).then(data => {
         // })
 
-        const y = d3.scaleLinear().domain([0,d3.max(datarr,d=>d.points)]).range([0,HEIGHT])
+        const y = d3.scaleLinear().domain([d3.min(datarr,d=>d.points),d3.max(datarr,d=>d.points)]).range([HEIGHT,0])
         const x = d3.scaleBand().domain(datarr.map(d=>d.name)).range([0,WIDTH]).padding(0.4)
+        const xAxisCall = d3.axisBottom(x)
+        const yAxisCall = d3.axisLeft(y)
+
+        svg.append("g")
+        .attr("transform",`translate(0, ${HEIGHT})`).call(xAxisCall)
+        svg.append("g").call(yAxisCall)
 
         const rectangles = svg.selectAll("rect").data(datarr)
 
